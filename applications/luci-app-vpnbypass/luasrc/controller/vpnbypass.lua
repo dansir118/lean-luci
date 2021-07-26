ced0d2ff99848ec34e738a1c2889f2a3e0d5155a
dansir118 <2683536526@qq.com>
2021-07-27 00:28:17 +0800

    luci-app-vpnbypass: luci菜单位置移至vpn层级

--- applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
+++ applications/luci-app-vpnbypass/luasrc/controller/vpnbypass.lua
@@ -3,5 +3,5 @@ function index()
 	if not nixio.fs.access("/etc/config/vpnbypass") then
 		return
 	end
-	entry({"admin", "services", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
+	entry({"admin", "vpn", "vpnbypass"}, cbi("vpnbypass"), _("VPN Bypass"))
 end
